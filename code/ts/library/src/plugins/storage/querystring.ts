ffd766e0869598af23e2fbfbc42ba75bd21a5432
Ben Croker <ben@putyourlightson.com>
2024-11-10 13:01:46 +0100

    Refactoring

--- code/ts/library/src/plugins/storage/querystring.ts
+++ code/ts/library/src/plugins/storage/querystring.ts
@@ -9,39 +9,30 @@ export const QueryStringAttributePlugin: AttributePlugin = {
     mustNotEmptyExpression: true,
 
     onLoad: (ctx) => {
-        // console.log("QueryStringAttributePlugin onLoad ", ctx);
+        const url = new URL(window.location.href);
+        const { expression, store } = ctx
+        const keys = expression.split(" ");
 
-        const keys = new Set<string>();
-        let expression = ctx.expression;
-        const parts = expression.split(" ");
-        for (const part of parts) {
-            keys.add(part);
-        }
-
-        if (keys.size > 0) {
-            const url = new URL(window.location.href);
-            const store = ctx.store();
-            const queryStringParams: Record<string, any> = {};
-            for (const key of keys) {
-                const parts = key.split(".");
-                let subQueryStringParams = queryStringParams;
-                let subStore = store;
-                for (let i = 0; i < parts.length - 1; i++) {
-                    const part = parts[i];
-                    if (!subQueryStringParams[part]) {
-                        subQueryStringParams[part] = {};
-                    }
-                    subQueryStringParams = subQueryStringParams[part];
+        for (const key of keys) {
+            const parts = key.split(".");
+            const params: Record<string, any> = {};
+            let subParams = params;
+            let subStore = store;
+            for (let i = 0; i < parts.length - 1; i++) {
+                const part = parts[i];
+                if (!subParams[part]) {
+                    subParams[part] = {};
                 }
-
-                const lastPart = parts[parts.length - 1];
-                subQueryStringParams[lastPart] = subStore[lastPart];
-
-                const marshalledStore = JSON.stringify(queryStringParams[key]);
-                url.searchParams.set(key, marshalledStore);
+                subParams = subParams[part];
             }
 
-            window.history.replaceState({}, '', url);
+            const lastPart = parts[parts.length - 1];
+            subParams[lastPart] = subStore[lastPart];
+
+            const queryString = new URLSearchParams(params).toString();
+            url.searchParams.set(key, queryString);
         }
+
+        window.history.replaceState({}, '', url);
     },
 };
