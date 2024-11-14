2b24534256023cdd354bca6db8f703d66b3edc57
Ben Croker <ben@putyourlightson.com>
2024-11-14 14:46:55 +0100

    v0.20.0, styling

--- code/go/sdk/consts.go
+++ code/go/sdk/consts.go
@@ -3,10 +3,10 @@ package datastar
 import "time"
 
 const (
-	Version                        = "0.20.0-beta"
-	VersionClientByteSize          = 43677
-	VersionClientByteSizeGzip      = 14902
-	VersionClientByteSizeGzipHuman = "15 KiB"
+	Version                        = "0.20.0"
+	VersionClientByteSize          = 43672
+	VersionClientByteSizeGzip      = 14899
+	VersionClientByteSizeGzipHuman = "14 KiB"
 
 	DefaultSettleTime = 300 * time.Millisecond
 	DefaultSseSendRetry = 1000 * time.Millisecond
