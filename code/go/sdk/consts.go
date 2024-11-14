650107ddeb8c20d5a30e35da3f1dee175ce18b29
Ben Croker <ben@putyourlightson.com>
2024-11-14 09:45:46 +0100

    SSE plugin fixes and code cleanup

--- code/go/sdk/consts.go
+++ code/go/sdk/consts.go
@@ -4,9 +4,9 @@ import "time"
 
 const (
 	Version                        = "0.20.0-beta"
-	VersionClientByteSize          = 43609
-	VersionClientByteSizeGzip      = 14889
-	VersionClientByteSizeGzipHuman = "14 KiB"
+	VersionClientByteSize          = 43677
+	VersionClientByteSizeGzip      = 14902
+	VersionClientByteSizeGzipHuman = "15 KiB"
 
 	DefaultSettleTime = 300 * time.Millisecond
 	DefaultSseSendRetry = 1000 * time.Millisecond
